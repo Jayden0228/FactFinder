dda859fa34cf32cb7101683092197808168306a0
Salrima Fernandes <salrima2001@gmail.com>
2023-05-27 20:41:43 +0530

    Added the frontend code

---
+++ static/index.js
@@ -0,0 +1,24 @@
+function displayform(){
+    document.getElementById('rect').style.display="block"
+    document.getElementById('text2').style.display="block"
+    document.getElementById('check').style.display="block"
+    document.getElementById('text').style.display="none"
+    document.getElementById('options').style.display="none"
+    document.getElementById('table').style.display="none"
+}
+function displayTable(){
+    document.getElementById('rect').style.display="none"
+    document.getElementById('text2').style.display="none"
+    document.getElementById('check').style.display="none"
+    document.getElementById('text').style.display="block"
+    document.getElementById('table').style.display="block"
+    document.getElementById('options').style.display="none"
+}
+function displayResults(){
+    document.getElementById('options').style.display="block"
+    document.getElementById('rect').style.display="none"
+    document.getElementById('text2').style.display="none"
+    document.getElementById('check').style.display="none"
+    document.getElementById('text').style.display="none"
+    document.getElementById('table').style.display="none"
+}
