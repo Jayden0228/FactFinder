c40f8f5ebce0d06bf709840e37659f1f66c199d1
Jayden Viegas <jaydenviegas57@gmail.com>
2023-05-29 20:33:32 +0530

    Input html added

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def Home():
-    return render_template("Index.html")
+    return render_template("index.html")
+
+@app.route('/input')
+def Input():
+    return render_template("input.html")
 
 
 if __name__=='__main__':
