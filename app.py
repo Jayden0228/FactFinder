b83cdf38fd637aa1a47d00b9c7ea6fb7963dc1eb
Jayden Viegas <jaydenviegas57@gmail.com>
2023-06-02 16:43:28 +0530

    Result endpoint updated

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
-from app.ML import ml
-from app.Scrapping import Scrape
+from mL.ml import fake_news
+from scrapping.Scrape import getHeadline 
 
 app = Flask(__name__)
 
@@ -14,16 +14,46 @@ def Input():
         btn=request.form['btntype']
 
         if btn=='headline':
-            return render_template("input.html", header='News Headline')
+            return render_template("input.html", header='News Headline', value='Headline')
         elif btn=='article':
-            return render_template("input.html", header='News Article Link')
+            return render_template("input.html", header='News Article Link', value='Article')
             
 
     # return render_template("input.html")
 
-@app.route('/result')
+@app.route('/result', methods=['POST'])
 def Result():
-    return render_template("result.html")
+    if request.method == 'POST':
+        texttype=request.form['texttype']
+        
+        if texttype=='Headline':
+            res=fake_news(request.form['textbox'])
+            
+            if res=="REAL":
+                src='../static/Images/real.png'
+                res="REAL"
+                return render_template("result.html", src=src, res=res)
+            elif res=="FAKE":
+                src='../static/Images/fake.png'
+                res="FAKE"
+                return render_template("result.html", src=src, res=res)
+        
+        elif texttype=='Article':
+            con=getHeadline(request.form['textbox'])
+            res=fake_news(con)
+
+            if res=="REAL":
+                src='../static/Images/real.png'
+                res="REAL"
+                return render_template("result.html", src=src, res=res)
+            elif res=="FAKE":
+                src='../static/Images/fake.png'
+                res="FAKE"
+                return render_template("result.html", src=src, res=res)
+            
+    return render_template("index.html")
+        
+    # return render_template("result.html")
 
 
 if __name__=='__main__':
